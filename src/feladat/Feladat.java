aff7b63e69aef9d6e83c50b600f373438a32be24
DDorka02 <dobszaydorka@gmail.com>
2024-02-28 16:17:49 +0100

    hatlom

--- src/feladat/Feladat.java
+++ src/feladat/Feladat.java
@@ -9,17 +9,23 @@ public class Feladat {
     
     public static void main(String[] args) {
         System.out.print("Kérek egy pozitiv számot: ");
-        int szam = sc.nextInt();
-        while (szam<1){
+        int dobasokszama = sc.nextInt();
+        while (dobasokszama<1){
             System.out.print("Hibás! kérek egy pozitív számot: ");
-            szam = sc.nextInt();
+            dobasokszama = sc.nextInt();
         }
-        final int MERET = 6;
-        int [] tomb = new int [MERET];
-        for (int i = 0; i < szam; i++) {
-            int h = (int) (Math.random()*6);
-        System.out.println(tomb[i] + h +"");
+        final int OLDALOKSZAMA = 6;
+        int [] tomb = new int [OLDALOKSZAMA+1];
+        for (int i = 0; i < dobasokszama; i++) {
+            int h = (int) (Math.random()*6+1);
+            h++;
         }
+        
+        for (int j=0;j<tomb.length ;j++){
+        
+        }
+        
+        
    
         }
 }
